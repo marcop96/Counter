c716d33c4f6686c7ae73e9235eec785285a6ded8
marcop96 <55959162+marcop96@users.noreply.github.com>
2022-06-04 19:33:40 +0200

    feature: decrease,reset,increase
    app now works correctly

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 let number = Number(document.querySelector("#value").innerText);
 let $numberspan = document.querySelector("#value");
-console.log(number);
 
 const $btnincrease = document.querySelector(".btn-increase");
 const $btnreset = document.querySelector(".btn-reset");
 const $btndecrease = document.querySelector(".btn-decrease");
 
 $btnincrease.addEventListener("click", function () {
-  number++;
-
+  number = number + 1;
   Number(($numberspan.innerText = number));
+  $numberspan.classList.add("positive");
+  $numberspan.classList.remove("negative");
 });
 
 $btndecrease.addEventListener("click", function () {
-  number--;
-
-  Number(($numberspan.innerText = number));
+  number = number - 1;
+  console.log(number);
+  $numberspan.innerText = number;
 });
+
 $btnreset.addEventListener("click", function () {
   number = 0;
   $numberspan.innerText = 0;
