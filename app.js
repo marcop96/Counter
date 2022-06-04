2fc6bec06d9fa26766b413f23819dedf1886a8d7
marcop96 <55959162+marcop96@users.noreply.github.com>
2022-06-04 19:37:49 +0200

    feature: color on the numberes
    now displays green when n is positive and red when n is negative

--- app.js
+++ app.js
@@ -8,17 +8,47 @@ const $btndecrease = document.querySelector(".btn-decrease");
 $btnincrease.addEventListener("click", function () {
   number = number + 1;
   Number(($numberspan.innerText = number));
-  $numberspan.classList.add("positive");
-  $numberspan.classList.remove("negative");
+  if (number == 0) {
+    $numberspan.classList.remove("positive", "negative");
+  }
+  if (number < 0) {
+    $numberspan.classList.remove("positive");
+    $numberspan.classList.add("negative");
+  }
+  if (number > 0) {
+    $numberspan.classList.add("positive");
+    $numberspan.classList.remove("negative");
+  }
 });
 
 $btndecrease.addEventListener("click", function () {
   number = number - 1;
-  console.log(number);
   $numberspan.innerText = number;
+  if (number == 0) {
+    $numberspan.classList.remove("positive", "negative");
+  }
+  if (number < 0) {
+    $numberspan.classList.remove("positive");
+    $numberspan.classList.add("negative");
+  }
+  if (number > 0) {
+    $numberspan.classList.add("positive");
+    $numberspan.classList.remove("negative");
+  }
 });
 
 $btnreset.addEventListener("click", function () {
   number = 0;
   $numberspan.innerText = 0;
+  if (number == 0) {
+    $numberspan.classList.remove("positive", "negative");
+  }
+  if (number < 0) {
+    $numberspan.classList.remove("positive");
+    $numberspan.classList.add("negative");
+  }
+  if (number > 0) {
+    $numberspan.classList.add("positive");
+    $numberspan.classList.remove("negative");
+  }
 });
